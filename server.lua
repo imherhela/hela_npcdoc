18ab87420dae74f89cbe007f287d7038704c76a0
ImHerHela <76200467+imherhela@users.noreply.github.com>
2026-03-18 06:06:10 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -142,3 +142,41 @@ function CheckTable(table, element)
     end
     return false
 end
+
+function checkUpdate(resource, repo)
+    local repos = repo:gsub("https://github.com/", "")
+
+    local current = GetResourceMetadata(resource, 'version')
+    PerformHttpRequest('https://raw.githubusercontent.com/'..repos..'/main/version', function(err, response, headers)
+        if not response or response == "" then
+            print("^1❕ Failed to check version file for " .. resource .. "^0")
+            return
+        end
+
+        local latest = response:match("<%d?%d.%d?%d.?%d?%d?>")
+        if latest then
+            latest = latest:gsub("[<>]", "")
+        else
+            print("^1❕ Invalid version format in the file for " .. resource .. "^0")
+            return
+        end
+
+        local update = latest > current
+
+        if update then
+            print("\n^8❌ -OUTDATED! ^6⋘ "..resource.."⋙ ^3(Version "..current..")^0")
+            print("^2New Version ^0("..latest..") ^5~"..repo.."~\n")
+            
+        else
+            print("\n^2✔️ -UPDATED! ^6⋘ "..resource.."⋙ ^3 (Version "..current..")\n")
+        end
+    end, 'GET', '', {
+        ['Content-Type'] = 'application/json'
+    })
+end
+
+AddEventHandler('onResourceStart', function(Resource)
+    if Resource == GetCurrentResourceName() then
+        checkUpdate(Resource, "https://github.com/imherhela/hela_npcdoc")
+    end
+end)
